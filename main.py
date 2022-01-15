8dc493d4821cf042901498439dc20773d38c7155
RafJan94 <rafjan1504@gmail.com>
2022-01-15 11:53:01 +0100

    Added file

--- main.py
+++ main.py
@@ -1,1 +1,84 @@
-print("hello world")
+# i = 2
+# print(type(i))
+# f = 2.76457
+# print(type(f))
+# c = 0.5+1j
+# print(type(c))
+# print(float(i), int(f), c)
+#
+# a = 2>1
+# print(a)
+# print(type(a))
+#
+# b = 2.568568 >3
+# print(type(b))
+#
+# znak = "A"
+# print(type(znak))
+#
+# napis = "Ala ma kota"
+# print(napis[::1])
+# print(napis[::2])
+# print(type(napis[::1]))
+#
+# zmienna = "127"*127
+# print(type(zmienna))
+#
+# napis = "Ala ma kota a kot ma psa"
+# liczba_calkowita = 2
+# print(napis + str(liczba_calkowita))
+#
+# str1 = "Datatypes"
+# str2 = "Fullstack"
+# print(str1[3:6], str2[3:6])
+# y = int(len(str1)*0.5)
+# z = int(len(str2)*0.5)
+# print(str1[y-1:y+2], str2[z-1:z+2])
+
+# s1 = "Fullstack"
+# s2 = "Python"
+# y = int(len(s1)/2)
+#
+# print(s1[:y]+s2+s1[y:])
+
+# s1 = "America"
+# s2 = "Japan"
+#
+# x = s1[0] + s2[0]
+# y = s1[-1]+s2[-1]
+#
+# z = s1[int(len(s1)/2)]+s2[int(len(s2)/2)]
+# print(x+z+y)
+
+# x = "Python"
+# print(x[::-1])
+
+# Jan = ("Jan", "Kowalski", 33)
+# Janina = ("Janina", "Nowak", (21, 12,1978), "K")
+# print(Jan, Janina)
+
+# lista = [1, 2, 3, 4, -5, 6, -10]
+# print(type(lista))
+
+# lista = [1, "Berta", 3, 4, -5, "kot", -10.75, 3.14]
+#
+# lista.insert(2, "zebra")
+# print(lista)
+
+# lista = ["Rafal", "Agata", "Michal", "Pawel", "Grzegorz", "Robert", "Aneta", "Tomasz", "Monika", "Klaudia", "Wiktor", "Kinga", "Marcin", "Tomasz", "Przemyslaw"]
+#
+# x = sorted(lista)
+#
+# print(len(x))
+# print(x)
+
+# tel = {}   #pusty
+# tel = {'Maja':57657, 'Jasio':463557}
+# tel["Ola"] = 3573577
+# print(tel)
+# del tel["Maja"]
+# print(tel)
+#
+# tel = dict([('Jan', 4139277), ('Kazimierz', 4126327)])
+# print(tel)
+
