b618513cce315888b290354fe67d4a53e0ecde68
RafJan94 <rafjan1504@gmail.com>
2022-01-17 18:57:04 +0100

    test

--- main.py
+++ main.py
@@ -259,8 +259,23 @@
 #     if numer % 7==0 and numer % 5 ==0:
 #         print(numer)
 
-def simple_function():
-    print('Hello world!')
-    print('Wikipedia')
+# def simple_function():
+#     print('Hello world!')
+#     print('Wikipedia')
+#
+# simple_function()
+#
+# def my_function():
+#     return 3+3
+#
+# print(my_function())
 
-simple_function()
+def fibbonaci_numbers(n):
+    ''' zwraca liczby Fibonacciego mniejsze od n '''
+    wynik = []
+    a, b = 0, 1
+    while a < n:
+    # while len(wynik) < n:
+        wynik.append(a)
+        a, b = b, a+b
+    return wynik
