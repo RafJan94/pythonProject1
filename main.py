9a9d275749bc6e05b7e083f80e6419dbc10af092
RafJan94 <rafjan1504@gmail.com>
2022-01-24 18:29:22 +0100

    szkolenie

--- main.py
+++ main.py
@@ -444,13 +444,53 @@
 # else:
 #     print("Ilość wierszy: " + len(f.readlines()))
 #     f.close()
-import TestR
-plik = TestR
-try:
-    plik = open("TestR.py", "r")
-    plik.write("string Lorum Ipsum")
-except IOError:
-    print("cos poszlo nie tak")
-finally:
-    plik.close()
+
+#
+# import TestR
+# plik = TestR
+# try:
+#     plik = open("TestR.py", "r")
+#     plik.write("string Lorum Ipsum")
+# except IOError:
+#     print("cos poszlo nie tak")
+# finally:
+#     plik.close()
+
+#   Praca na plikach
+
+# f = open('write_file_name', 'w')  # starą zawartość zastępujemy nową
+# f = open('append_file_name', 'a')
+# f = open('read_file_name', 'r')
+#
+# f.read()
+#
+# f.readline()
+
+# f.write('Witaj\n')
+# value = 42
+# f.write(value)
+#
+# >>>Traceback (most recent call last):
+# >>> File "<stdin>", line 1, in <module>
+# >>>TypeError: must be str, not int
+#
+# s = str(value)
+# f.write(s)
+
+
+# Otwórz plik
+
+fo = open("text.txt", "r")
+print("Nazwa pliku: ", fo.name)
+
+line = fo.readline()
+print("Czytaj linię: >" + line + "<")   #   teraz wskaźnik jest w następnej linii
+
+# Ponownie ustaw wskaźnik na początek
+fo.seek(0, 0)  # fo.seek(0)
+line = fo.readline()
+print("Czytaj linię: >" + line + "<")
+
+# Zamknij otwarty plik
+fo.close()
 
