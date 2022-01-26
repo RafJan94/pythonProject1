738bbfcb0b5346655e5951ba8f2281bb15795c9b
RafJan94 <rafjan1504@gmail.com>
2022-01-26 19:32:19 +0100

    szkolenie

--- main.py
+++ main.py
@@ -594,11 +594,101 @@
 # a = sin(pi/2)
 # print(a)
 
-def lemon(l):   #   tworzy moduł fruit
-    print('Lemoniada nr', l)
+# def lemon(l):   #   tworzy moduł fruit
+#     print('Lemoniada nr', l)
+#
+# import fruit
+# # from fruit import lemon
+#
+# fruit.lemon(5)
+# # lemon(5)
 
-import fruit
-# from fruit import lemon
+#       funkcje anonimowe
 
-fruit.lemon(5)
-# lemon(5)
+# pitagoras = lambda a, b:  ((a * a) + (b * b)) ** 0.5
+# print(pitagoras(3, 4))
+
+# def c_sum(x, y):
+#     return x + y
+#
+# l_sum = lambda x, y: x+y
+#
+# print(c_sum(3, 4))
+# print(l_sum(3, 4))
+
+#       Filtrowanie, aplikowanie, redukowanie
+
+temperatury = [
+    37.6, 35.8, 37.6, 33.4, 34.1, 37.1, 35.9, 34.1, 37.1, 40.5, 38.5, 37.6,
+    35.8, 34.5, 36.4, 38.3, 37.5, 37.7, 34.0, 35.3, 35.7, 38.9, 34.8, 34.1,
+    39.6, 35.4, 34.7, 37.6, 38.4, 36.4, 39.8, 39.1, 37.1, 35.6, 36.8, 37.6,
+    36.7, 40.0, 38.0, 34.1, 35.5, 38.5, 36.1, 32.6, 32.9, 34.5, 41.0, 38.3,
+    33.7, 38.7, 36.9, 36.2, 33.7, 38.3, 35.3, 38.3, 40.1, 39.3, 38.2, 37.6,
+    39.1, 37.1, 34.4, 38.7, 35.8, 38.2, 38.2, 33.1, 37.8, 36.5, 37.6, 37.4,
+    34.3, 37.7, 36.0, 37.5, 37.6, 36.5, 31.3, 37.7, 40.3, 39.5, 35.7, 38.1,
+    34.7, 36.5, 34.3, 38.0, 37.0, 38.5, 39.4, 37.6, 41.7, 40.0, 38.4, 38.9,
+    34.2, 40.2, 34.3, 35.3
+]
+# wynik = list(filter(lambda x: x >= 40.0, temperatury))
+# print(wynik)
+# wynik_sort = sorted(wynik)
+# print(wynik_sort)
+#
+# from statistics import mean   #srednia
+# st_temp = mean(temperatury)
+# print(st_temp)
+#
+# odch = list(map(lambda x: round(x - st_temp,1), temperatury))
+# print(odch)
+
+# from functools import reduce        #redukuje do jednej wartości
+# nums = [1, 2, 3, 4, 5]      #   działanie reduce ((((1+2)+3)+4)+5)
+# print(reduce(lambda a, b: a + b, nums))
+
+        #       list comprehension
+# szesciany = []
+# for x in range(10):
+#     szesciany.append(x**3)
+# print(szesciany)
+
+        #   lub
+
+# szesciany = [x**3 for x in range(10)]   #   tworzenie listy
+# print(szesciany)
+
+# kwadraty = [el**2 for el in range(1, 102) if el % 2 != 0]
+# print(kwadraty)
+
+# zbior = {znak for znak in "abracadabra" if znak not in "abc"}
+# print(zbior)
+#
+# tekst = "abracadabra"
+# wystapienia = {znak: tekst.count(znak) for znak in tekst}
+# print(wystapienia)
+#
+# c = {x: x**2 for x in (2, 4, 6)}  # {2: 4, 4: 16, 6: 36}
+# print(c)
+#
+# list_comp = [x ** 0.5 for x in range(1, 11)]
+# gene_expr = (x ** 0.5 for x in range(1, 11))
+#
+# for x in list_comp:
+#     print(x)
+# for x in gene_expr:
+#     print(x)
+#
+# list_comp = [x ** 0.5 for x in range(1, 50000001)]
+# sum = 0
+# for x in list_comp:
+#     sum += x
+# print(sum)
+#
+# gene_expr = (x ** 0.5 for x in range(1, 50000001))
+# # gene_expr nie obliczyło jeszcze tych milionów pierwiastków
+#
+# sum = 0
+# for x in gene_expr:
+#     # teraz już kolejne elementy wyrażenia generatorowego są obliczane
+#     sum += x
+# print(sum)
+
